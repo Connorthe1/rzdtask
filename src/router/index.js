02630377ba9577805bb79fd53a726dd981ece3d4
Andrey Melnik <www.mel77@mail.ru>
2022-05-29 02:48:31 +0300

    init commit

--- src/router/index.js
+++ src/router/index.js
@@ -11,17 +11,15 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/history',
+    name: 'History',
+    component: () => import('../views/History.vue')
   }
 ]
 
 const router = new VueRouter({
-  routes
+  mode: 'history',
+  routes,
 })
 
 export default router
